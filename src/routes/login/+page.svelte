14705f1aa3845f7c5f845612999ec8127b54c469
Michael <202211836@gordoncollege.edu.ph>
2024-11-05 22:40:22 +0800

    Initial Commit

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -17,7 +17,7 @@
 
 <div class="flex min-h-screen">
     <div class="flex-1 bg-green-800 flex items-center justify-center">
-        <img src="image1.png" alt="Fallback description if image fails to load" class="max-w-full h-auto" aria-hidden="true" />
+        <img src="./image1.png" alt="Fallback description if image fails to load" class="max-w-full h-auto" aria-hidden="true" />
     </div>
     <div class="flex-1 flex items-center justify-center">
         <form on:submit|preventDefault={handleSubmit} class="p-6 w-2/3">
